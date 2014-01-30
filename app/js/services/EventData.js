da266768d2238b9791ab3d3d704b32c36b802610
Ahsan <vampxlr@yahoo.com>
2014-01-30 14:15:04 +0600

    / root dir change

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -3,7 +3,7 @@
 eventsApp.factory('eventData',function($http,$log){
    return{
        getEvent:function(successcb){
-           $http({method:'GET',url:'data/event/1.rawdata'})
+           $http({method:'GET',url:'data/event/1.xml'})
                .success(function(data,status,headers,config){
                     successcb(data);
                    $log.info(data,status,headers,config);
